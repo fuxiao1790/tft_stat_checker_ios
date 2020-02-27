eb9792e59e17a0db6b81a0d27ac014473ac15eb0
Ken <xiao.fu1790@gmail.com>
2020-02-27 15:09:34 -0500

    Initial Commit

---
+++ tft_stat_checker_ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tft_stat_checker_ios
+//
+//  Created by fuxiao on 2020-02-27.
+//  Copyright © 2020 fuxiao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tft_stat_checker_iosTests/tft_stat_checker_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  tft_stat_checker_iosTests.swift
+//  tft_stat_checker_iosTests
+//
+//  Created by fuxiao on 2020-02-27.
+//  Copyright © 2020 fuxiao. All rights reserved.
+//
+
+import XCTest
+@testable import tft_stat_checker_ios
+
+class tft_stat_checker_iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
